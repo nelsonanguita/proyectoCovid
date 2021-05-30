52dfb57f5f1f2669d411ee73984e3463f5af02e9
nelson anguita <nelsonanguita@gmail.com>
2021-05-30 13:03:08 -0400

    responsive, modo oscuro,

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,20 +3,20 @@
     <NavBar />
 <v-container  fluid>
   <v-responsive>  
-        <h1>Información COVID Chile --</h1>
+        <h1>Información COVID Chile</h1>
 
-  <v-row class="mx-auto ">
-       <v-col  xs="12" sm="12" md="3" xl="3">
+  <v-row >
+       <v-col class="mx-auto " xs="12" sm="12" md="3" xl="3">
           <InfoDiaria />
        </v-col>
 
 
-      <v-col xs="12" sm="12" md="6" xl="6">
+      <v-col class="mx-auto " xs="12" sm="12" md="6" xl="6">
           <Grafico />
       </v-col>
 
 
-        <v-col xs="12" sm="12" md="3" xl="3">
+        <v-col class="mx-auto " xs="12" sm="12" md="3" xl="3">
           <Comunas10 />
         </v-col>
     </v-row>
