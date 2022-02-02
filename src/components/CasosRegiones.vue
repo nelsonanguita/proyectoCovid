3d1d00a7f7dddd8fb7def816864a152caa1404c5
nelson anguita <nelsonanguita@gmail.com>
2022-02-02 19:22:27 -0300

    Casos por region

--- src/components/CasosRegiones.vue
+++ src/components/CasosRegiones.vue
@@ -118,7 +118,7 @@ export default {
     async obtenerDatosRegiones() {
       try {
         let datos = await axios.get(
-          "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto4/2022-01-30-CasosConfirmados-totalRegional.csv"
+          "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto4/2022-02-02-CasosConfirmados-totalRegional.csv"
         );
         let lines = datos.data.split("\n");
         for (let i = 1 ; i < lines.length; i++) {
